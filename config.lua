f584a838614eec2db516de7d7cbd8cc3ad71166c
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-08-27 22:56:21 -0400

    REMOVED MECHANICS FROM RADIO

--- config.lua
+++ config.lua
@@ -4,61 +4,51 @@ Config.RestrictedChannels = {
     [1] = {
         police = true,
         ambulance = true,
-        mechanic = true,
         firefighter = true
     },
     [2] = {
         police = true,
         ambulance = true,
-        mechanic = true,
         firefighter = true
     },
     [3] = {
         police = true,
         ambulance = true,
-        mechanic = true,
         firefighter = true
     },
     [4] = {
         police = true,
         ambulance = true,
-        mechanic = true,
         firefighter = true
     },
     [5] = {
         police = true,
         ambulance = true,
-        mechanic = true,
         firefighter = true
     },
     [6] = {
         police = true,
         ambulance = true,
-        mechanic = true,
         firefighter = true
     },
     [7] = {
         police = true,
         ambulance = true,
-        mechanic = true,
         firefighter = true
     },
     [8] = {
         police = true,
         ambulance = true,
-        mechanic = true,
         firefighter = true
     },
     [9] = {
         police = true,
         ambulance = true,
-        mechanic = true,
         firefighter = true
     },
     [10] = {
         police = true,
         ambulance = true,
-        mechanic = true,
         firefighter = true
     }
 }
