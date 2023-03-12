3e6184189db624a16d132446fc4d656715c5b235
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-03-11 21:06:23 -0500

    added leo departments

--- config.lua
+++ config.lua
@@ -3,51 +3,81 @@ Config = {}
 Config.RestrictedChannels = {
     [1] = {
         police = true,
+        bcso = true,
+        sasp = true,
+        sapr = true,
         ambulance = true,
         firefighter = true
     },
     [2] = {
         police = true,
+        bcso = true,
+        sasp = true,
+        sapr = true,
         ambulance = true,
         firefighter = true
     },
     [3] = {
         police = true,
+        bcso = true,
+        sasp = true,
+        sapr = true,
         ambulance = true,
         firefighter = true
     },
     [4] = {
         police = true,
+        bcso = true,
+        sasp = true,
+        sapr = true,
         ambulance = true,
         firefighter = true
     },
     [5] = {
         police = true,
+        bcso = true,
+        sasp = true,
+        sapr = true,
         ambulance = true,
         firefighter = true
     },
     [6] = {
         police = true,
+        bcso = true,
+        sasp = true,
+        sapr = true,
         ambulance = true,
         firefighter = true
     },
     [7] = {
         police = true,
+        bcso = true,
+        sasp = true,
+        sapr = true,
         ambulance = true,
         firefighter = true
     },
     [8] = {
         police = true,
+        bcso = true,
+        sasp = true,
+        sapr = true,
         ambulance = true,
         firefighter = true
     },
     [9] = {
         police = true,
+        bcso = true,
+        sasp = true,
+        sapr = true,
         ambulance = true,
         firefighter = true
     },
     [10] = {
         police = true,
+        bcso = true,
+        sasp = true,
+        sapr = true,
         ambulance = true,
         firefighter = true
     }
